62c33c9173d7f3f42b9cf0d6ed48c1e71d6d3dc7
webtec-cloud <dreamserve@googlemail.com>
2022-04-28 16:15:39 +0100

    feat: our card component created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "./App.css";
+import { Button, Stack } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import BudgetCard from "./components/BugetCard";
 
 const App = () => {
-  return <div>Budget App</div>;
+  return (
+    <>
+      <Container className="my-5">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BudgetCard = ({ name, amount, max }) => {
+  return <div>BudgetCard</div>;
+};
+
+export default BudgetCard;
